5d9d33f816c0c23ea2e48efd3c9c1543c77bbef0
OneDev0411 <cryptoapple1109@gmail.com>
2023-05-15 14:55:39 +0500

    removing Siekiq::Status stuff

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,9 +2,6 @@ require 'sidekiq'
 require 'sidekiq/api'
 
 Sidekiq.configure_client do |config|
-  # accepts :expiration (optional)
-  Sidekiq::Status.configure_client_middleware config, expiration: 30.minutes
-
   config.redis = {
     url: ENV["REDIS_URL"],
     ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
@@ -12,12 +9,6 @@ Sidekiq.configure_client do |config|
 end
 
 Sidekiq.configure_server do |config|
-  # accepts :expiration (optional)
-  Sidekiq::Status.configure_server_middleware config, expiration: 30.minutes
-
-  # accepts :expiration (optional)
-  Sidekiq::Status.configure_client_middleware config, expiration: 30.minutes
-
   config.redis = {
     url: ENV["REDIS_URL"],
     ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
