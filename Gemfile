ed51a5003722aab44e42bec176ab4c01601d1907
OneDev0411 <cryptoapple1109@gmail.com>
2023-05-15 14:12:09 +0500

    upgrading redis + adding sidekiq configuration for redis

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 gem 'rollbar'
 gem 'delayed_job_active_record'
 gem 'pg', '~> 1.4', '>= 1.4.5'
+gem 'redis'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,2 +1,25 @@
 require 'sidekiq'
 require 'sidekiq/api'
+
+Sidekiq.configure_client do |config|
+  # accepts :expiration (optional)
+  Sidekiq::Status.configure_client_middleware config, expiration: 30.minutes
+
+  config.redis = {
+    url: ENV["REDIS_URL"],
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
+end
+
+Sidekiq.configure_server do |config|
+  # accepts :expiration (optional)
+  Sidekiq::Status.configure_server_middleware config, expiration: 30.minutes
+
+  # accepts :expiration (optional)
+  Sidekiq::Status.configure_client_middleware config, expiration: 30.minutes
+
+  config.redis = {
+    url: ENV["REDIS_URL"],
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
+end
