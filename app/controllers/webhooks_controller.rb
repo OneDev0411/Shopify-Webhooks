312ec3bc020e3073a3ae693bc0c7b3feb9aa4bb6
OneDev0411 <cryptoapple1109@gmail.com>
2023-09-18 22:05:14 +0500

    Remove rollbar logger

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -36,38 +36,32 @@ class WebhooksController < ApplicationController
   end
 
   def order
-    begin
-      if params[:detail].present?
-        payload = params[:detail][:payload]
-      else
-        payload = params[:webhook]
-      end
-      discount_code = if payload['discount_codes'] && payload['discount_codes'][0]
-        payload['discount_codes'][0]['code']
-      else
-        nil
-      end
-      order_opts = {
-        shopify_id: payload['id'],
-        items: (payload['line_items'] || []).map{|l| l['product_id'] }.compact.sort,
-        item_variants: (payload['line_items'] || []).map{|l| { variant_id: l['variant_id'], quantity: l['quantity'], price: l['price'], discount: l['discount_allocations'] } },
-        discount_code: discount_code,
-        shopper_country: payload['billing_address'].present? ? payload['billing_address']['country_code'] : nil,
-        referring_site: payload['referring_site'],
-        orders_count: payload['customer'].present? ? payload['customer']['orders_count'] : nil,
-        total: payload['total_price'],
-        cart_token: payload['cart_token']
-      }
-      Sidekiq::Client.push('class' => 'ShopWorker::RecordOrderJob', 'args' => [@myshopify_domain, order_opts], 'queue' => 'low', 'at' => Time.now.to_i + 10)
-      Rollbar.info("Pushed ShopWorker::RecordOrderJob in queue for shop #{@myshopify_domain} and order #{order_opts}");
-      unless payload['cart_token'].nil?
-        Sidekiq::Client.push('class' => 'ShopWorker::SaveOfferSaleJob', 'args' => [order_opts], 'queue' => 'sale_stats', 'at' => Time.now.to_i + 11)
-        Rollbar.info("Pushed ShopWorker::SaveOfferSaleJob in queue for shop #{@myshopify_domain} and order #{order_opts}");
-      end
-      head :ok and return
-    rescue StandardError => e
-      Rollbar.error("Error", e)
+    if params[:detail].present?
+      payload = params[:detail][:payload]
+    else
+      payload = params[:webhook]
     end
+    discount_code = if payload['discount_codes'] && payload['discount_codes'][0]
+      payload['discount_codes'][0]['code']
+    else
+      nil
+    end
+    order_opts = {
+      shopify_id: payload['id'],
+      items: (payload['line_items'] || []).map{|l| l['product_id'] }.compact.sort,
+      item_variants: (payload['line_items'] || []).map{|l| { variant_id: l['variant_id'], quantity: l['quantity'], price: l['price'], discount: l['discount_allocations'] } },
+      discount_code: discount_code,
+      shopper_country: payload['billing_address'].present? ? payload['billing_address']['country_code'] : nil,
+      referring_site: payload['referring_site'],
+      orders_count: payload['customer'].present? ? payload['customer']['orders_count'] : nil,
+      total: payload['total_price'],
+      cart_token: payload['cart_token']
+    }
+    Sidekiq::Client.push('class' => 'ShopWorker::RecordOrderJob', 'args' => [@myshopify_domain, order_opts], 'queue' => 'low', 'at' => Time.now.to_i + 10)
+    unless payload['cart_token'].nil?
+      Sidekiq::Client.push('class' => 'ShopWorker::SaveOfferSaleJob', 'args' => [order_opts], 'queue' => 'sale_stats', 'at' => Time.now.to_i + 11)
+    end
+    head :ok and return
   end
 
   def app_uninstalled
